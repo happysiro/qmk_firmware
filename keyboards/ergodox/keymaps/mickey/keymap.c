225b1cebc49d0ba27b81c343c6ca9ceb224192c6
mickey <mitsuki@pepabo.com>
2016-09-30 14:11:27 +0900

    keymap をカスタマイズ

--- keyboards/ergodox/keymaps/mickey/keymap.c
+++ keyboards/ergodox/keymaps/mickey/keymap.c
@@ -33,22 +33,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_MINS,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MO(1),
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_DOWN,
         KC_LCTL,        LT(MDIA,KC_A),KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
         LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  ALT_T(KC_APP),  KC_LGUI,
-                                               KC_ESC,MO(2),
-                                                              KC_HOME,
-                                               KC_SPC,KC_LCTL,KC_END,
+                                                      KC_ESC, KC_FN1,
+                                                              MO(2),
+                                               KC_SPC,KC_LCTL,MO(1),
         // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_EQL,
-             MO(1),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,
-             KC_BSPC,KC_N,   KC_M,        KC_COMM,KC_DOT,  KC_SLSH, KC_RSFT,
-                                  KC_RGUI,KC_RALT,KC_LBRC,KC_RBRC,          KC_FN1,
-             MO(2),KC_ESC,
-             KC_PGUP,
-             KC_PGDN,KC_RCTL, KC_ENT
+             KC_UP,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
+             KC_BSPC,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+             KC_RGUI,     KC_RALT,KC_LBRC,KC_RBRC,          KC_FN1,
+             KC_FN1,KC_ESC,
+             MO(2),
+             MO(1),KC_RCTL, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
